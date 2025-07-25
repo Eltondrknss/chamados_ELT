55f1c56b1b9baaa61e2362902d2f04b071488ade
Eltondrknss <eltonoliveira1988@gmail.com>
2025-07-25 11:03:11 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
-class Chamado:
-    def __init__(self, titulo, descricao, solicitante, status = 'Aberto', id = None):
-        self.id = id
-        self.titulo = titulo
-        self.descricao = descricao
-        self.solicitante = solicitante
-        self.status = status
+# class Chamado:
+#     def __init__(self, titulo, descricao, solicitante, status = 'Aberto', id = None):
+#         self.id = id
+#         self.titulo = titulo
+#         self.descricao = descricao
+#         self.solicitante = solicitante
+#         self.status = status
         
-    def __repr__(self):
-        return f"Chamado(ID: {self.id}, Titulo: {self.titulo}, Solicitante: {self.solicitante}, Status: {self.status})"
+#     def __repr__(self):
+#         return f"Chamado(ID: {self.id}, Titulo: {self.titulo}, Solicitante: {self.solicitante}, Status: {self.status})"
