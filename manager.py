cec15321d51a79aee876f1c4b4296367ec96e7a5
Eltondrknss <eltonoliveira1988@gmail.com>
2025-07-25 11:00:56 -0300

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,8 +1,11 @@
 import database
 
+# classe que abriga as funções de chamados
 class GestorDeChamados:
 
+    # cria um novo chamado
     def criar_novo_chamado(self, titulo, descricao, solicitante):
+        # Retorna erro se campos obrigatórios não forem preenchidos
         if not titulo or not descricao or not solicitante:
             print(">>>> ERRO: Todos os campos são obrigatórios")
             return False
@@ -11,19 +14,23 @@ class GestorDeChamados:
         if sucesso:
             print(">>>> Chamado aberto com sucesso!")
             return sucesso
-    
+
+    # chama a função que lista todos os chamados
     def consultar_todos_chamados(self):
         return database.listar_todos_chamados()
-    
+
+    # retorna o chamado referente ao ID inserido
     def encontrar_chamado_por_id(self, id_chamado):
         return database.buscar_chamado_por_ID(id_chamado)
-    
+
+    # recebe os dados para atualizar o status do chamado com o ID digitado
     def atualizar_status_do_chamado(self, id_chamado, novo_status):
         chamado_existente = self.encontrar_chamado_por_id(id_chamado)
+        #retorna erro caso ID não exista
         if not chamado_existente:
             print(f">>>> ERRO: Chamado com o ID {id_chamado} não encontrado")
             return False
-        
+        #retorna erro caso ID não seja enviado
         if not novo_status:
             print(">>>> ERRO: O novo status não pode ser vazio")
             return False
@@ -32,9 +39,11 @@ class GestorDeChamados:
         if sucesso:
             print(f"Status do chamado {id_chamado} atualizado com sucesso!")
             return sucesso
-        
+
+    # deleta um chamado pelo ID
     def deletar_chamado_por_id(self, id_chamado):
         chamado_existente  = self.encontrar_chamado_por_id(id_chamado)
+        #retorna erro caso ID não exista
         if not chamado_existente:
             print(f">>>> ERRO: Chamado com o ID {id_chamado} não encontrado.")
             return False
